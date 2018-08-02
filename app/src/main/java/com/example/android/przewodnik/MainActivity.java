8ccd1a96a44a276e80aa86a5e2a9fb6f29886b48
tr0lczyk <matt.olczyk@gmail.com>
2018-08-02 23:43:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/przewodnik/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.przewodnik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ustka_main);
+    }
+}
